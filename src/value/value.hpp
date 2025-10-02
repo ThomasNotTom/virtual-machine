a5ec5a1878319e0b3b0011d02f0b32986261d935
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-02 22:43:19 +0100

    Create value class

---
+++ src/value/value.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstdint>
+
+#include "../memory/memory.hpp"
+
+class Value {
+private:
+  uint64_t hops;
+  uint64_t data;
+
+public:
+  Value(uint64_t data) : data(data), hops(0) {};
+
+  bool hop(const Memory& memory, uint64_t& out) {
+    if (this->hops == 0) {
+      return false;
+    }
+
+    memory.get64(this->data, this->data);
+    this->hops--;
+    return true;
+  };
+
+  uint64_t getData() const { return this->data; }
+};
