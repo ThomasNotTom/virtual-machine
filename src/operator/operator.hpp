1c451c22035cb9c330d43f3b2fba422b0a6d971f
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-02 22:42:57 +0100

    Create operator class

---
+++ src/operator/operator.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "../memory/memory.hpp"
+#include "../stack_pointer/stack_pointer.hpp"
+
+#include "../value/value.hpp"
+
+class Operator {
+protected:
+  const Value arg1;
+  const Value arg2;
+
+public:
+  Operator(const Value& arg1, const Value& arg2) : arg1(arg1), arg2(arg2) {};
+  virtual uint64_t operate(Memory& memory, StackPointer& sp,
+                           const Value& out) = 0;
+};
