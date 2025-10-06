62149a9884bad99e435a9fd7822ab725ed730a55
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-06 16:38:38 +0100

    Add comments to run method

--- src/instructions/instructions.cpp
+++ src/instructions/instructions.cpp
@@ -6,14 +6,16 @@ Instructions::Instructions(std::vector<Instruction> code)
     : code(std::move(code)) {};
 
 bool Instructions::run(StackPointer& sp, Memory& memory) {
+  // Has recieved a halting instruction
   bool halt = false;
-  uint64_t i = 0;
   while (!halt) {
     if (sp.getAddress() >= this->code.size()) {
+      // Return a non-graceful finish
       return false;
     }
     Instruction& operation = this->code[sp.getAddress()];
     halt = operation.execute(sp, memory);
   }
+  // Return a graceful finish
   return true;
 }
