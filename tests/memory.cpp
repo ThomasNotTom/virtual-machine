59839f5a4c81e9243b13b27a279b0264dfad580b
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-06 15:46:10 +0100

    Include temporary value in test

--- tests/memory.cpp
+++ tests/memory.cpp
@@ -7,7 +7,8 @@
 TEST_CASE("Memory set and get 1 byte value", "[memory]") {
   const uint8_t TEST_VALUE = UINT8_MAX;
   Memory memory(1);
-  memory.set8(0, TEST_VALUE);
+  Value _;
+  memory.set8(0, TEST_VALUE, _);
   uint8_t value;
   memory.get8(0, value);
   REQUIRE(value == TEST_VALUE);
