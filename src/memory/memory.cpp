342a7b20a111ecab4275a99c169fc8e0f3870c59
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-06 16:12:26 +0100

    Move implementation

--- src/memory/memory.cpp
+++ src/memory/memory.cpp
@@ -11,3 +11,101 @@ bool Memory::get8(const uint64_t location, uint8_t& out) const {
 
   return true;
 }
+
+bool Memory::set8(const uint64_t location, const uint8_t in, Value& value) {
+  if (location > this->data.size()) {
+    return false;
+  }
+
+  this->data[location] = in;
+
+  value = Value(location, 1);
+  return true;
+}
+
+bool Memory::get16(const uint64_t location, uint16_t& out) const {
+  if ((location + 1) > this->data.size()) {
+    return false;
+  }
+
+  ((uint8_t*)&out)[0] = this->data[location];
+  ((uint8_t*)&out)[1] = this->data[location + 1];
+
+  return true;
+}
+
+bool Memory::set16(const uint64_t location, const uint16_t in) {
+  if ((location + 1) > this->data.size()) {
+    return false;
+  }
+
+  this->data[location] = ((uint8_t*)&in)[0];
+  this->data[location + 1] = ((uint8_t*)&in)[1];
+  return true;
+}
+
+bool Memory::get32(const uint64_t location, uint32_t& out) const {
+  if ((location + 3) > this->data.size()) {
+    return false;
+  }
+
+  ((uint8_t*)&out)[0] = this->data[location];
+  ((uint8_t*)&out)[1] = this->data[location + 1];
+  ((uint8_t*)&out)[2] = this->data[location + 2];
+  ((uint8_t*)&out)[3] = this->data[location + 3];
+
+  return true;
+}
+
+bool Memory::set32(const uint64_t location, const uint32_t in) {
+  if ((location + 3) > this->data.size()) {
+    return false;
+  }
+
+  this->data[location] = ((uint8_t*)&in)[0];
+  this->data[location + 1] = ((uint8_t*)&in)[1];
+  this->data[location + 2] = ((uint8_t*)&in)[2];
+  this->data[location + 3] = ((uint8_t*)&in)[3];
+  return true;
+}
+
+bool Memory::get64(const uint64_t location, uint64_t& out) const {
+  if ((location + 7) > this->data.size()) {
+    return false;
+  }
+
+  ((uint8_t*)&out)[0] = this->data[location];
+  ((uint8_t*)&out)[1] = this->data[location + 1];
+  ((uint8_t*)&out)[2] = this->data[location + 2];
+  ((uint8_t*)&out)[3] = this->data[location + 3];
+  ((uint8_t*)&out)[4] = this->data[location + 4];
+  ((uint8_t*)&out)[5] = this->data[location + 5];
+  ((uint8_t*)&out)[6] = this->data[location + 6];
+  ((uint8_t*)&out)[7] = this->data[location + 7];
+
+  return true;
+}
+
+bool Memory::set64(const uint64_t location, const uint64_t in) {
+  if ((location + 7) > this->data.size()) {
+    return false;
+  }
+
+  this->data[location] = ((uint8_t*)&in)[0];
+  this->data[location + 1] = ((uint8_t*)&in)[1];
+  this->data[location + 2] = ((uint8_t*)&in)[2];
+  this->data[location + 3] = ((uint8_t*)&in)[3];
+  this->data[location + 4] = ((uint8_t*)&in)[4];
+  this->data[location + 5] = ((uint8_t*)&in)[5];
+  this->data[location + 6] = ((uint8_t*)&in)[6];
+  this->data[location + 7] = ((uint8_t*)&in)[7];
+  return true;
+}
+
+void Memory ::print() const {
+  std::cout << "-- Memory (" << this->data.size() << ") --" << std::endl;
+
+  for (uint64_t i = 0; i < this->data.size(); i++) {
+    std::cout << (uint16_t)this->data[i] << std::endl;
+  }
+}
--- src/memory/memory.hpp
+++ src/memory/memory.hpp
@@ -13,102 +13,16 @@ private:
 public:
   Memory(uint64_t size);
   bool get8(const uint64_t location, uint8_t& out) const;
+  bool set8(const uint64_t location, const uint8_t in, Value& value);
 
-  bool set8(const uint64_t location, const uint8_t in, Value& value) {
-    if (location > this->data.size()) {
-      return false;
-    }
+  bool get16(const uint64_t location, uint16_t& out) const;
+  bool set16(const uint64_t location, const uint16_t in);
 
-    this->data[location] = in;
+  bool get32(const uint64_t location, uint32_t& out) const;
+  bool set32(const uint64_t location, const uint32_t in);
 
-    value = Value(location, 1);
-    return true;
-  }
+  bool get64(const uint64_t location, uint64_t& out) const;
+  bool set64(const uint64_t location, const uint64_t in);
 
-  bool get16(const uint64_t location, uint16_t& out) const {
-    if ((location + 1) > this->data.size()) {
-      return false;
-    }
-
-    ((uint8_t*)&out)[0] = this->data[location];
-    ((uint8_t*)&out)[1] = this->data[location + 1];
-
-    return true;
-  }
-
-  bool set16(const uint64_t location, const uint16_t in) {
-    if ((location + 1) > this->data.size()) {
-      return false;
-    }
-
-    this->data[location] = ((uint8_t*)&in)[0];
-    this->data[location + 1] = ((uint8_t*)&in)[1];
-    return true;
-  }
-
-  bool get32(const uint64_t location, uint32_t& out) const {
-    if ((location + 3) > this->data.size()) {
-      return false;
-    }
-
-    ((uint8_t*)&out)[0] = this->data[location];
-    ((uint8_t*)&out)[1] = this->data[location + 1];
-    ((uint8_t*)&out)[2] = this->data[location + 2];
-    ((uint8_t*)&out)[3] = this->data[location + 3];
-
-    return true;
-  }
-
-  bool set32(const uint64_t location, const uint32_t in) {
-    if ((location + 3) > this->data.size()) {
-      return false;
-    }
-
-    this->data[location] = ((uint8_t*)&in)[0];
-    this->data[location + 1] = ((uint8_t*)&in)[1];
-    this->data[location + 2] = ((uint8_t*)&in)[2];
-    this->data[location + 3] = ((uint8_t*)&in)[3];
-    return true;
-  }
-
-  bool get64(const uint64_t location, uint64_t& out) const {
-    if ((location + 7) > this->data.size()) {
-      return false;
-    }
-
-    ((uint8_t*)&out)[0] = this->data[location];
-    ((uint8_t*)&out)[1] = this->data[location + 1];
-    ((uint8_t*)&out)[2] = this->data[location + 2];
-    ((uint8_t*)&out)[3] = this->data[location + 3];
-    ((uint8_t*)&out)[4] = this->data[location + 4];
-    ((uint8_t*)&out)[5] = this->data[location + 5];
-    ((uint8_t*)&out)[6] = this->data[location + 6];
-    ((uint8_t*)&out)[7] = this->data[location + 7];
-
-    return true;
-  }
-
-  bool set64(const uint64_t location, const uint64_t in) {
-    if ((location + 7) > this->data.size()) {
-      return false;
-    }
-
-    this->data[location] = ((uint8_t*)&in)[0];
-    this->data[location + 1] = ((uint8_t*)&in)[1];
-    this->data[location + 2] = ((uint8_t*)&in)[2];
-    this->data[location + 3] = ((uint8_t*)&in)[3];
-    this->data[location + 4] = ((uint8_t*)&in)[4];
-    this->data[location + 5] = ((uint8_t*)&in)[5];
-    this->data[location + 6] = ((uint8_t*)&in)[6];
-    this->data[location + 7] = ((uint8_t*)&in)[7];
-    return true;
-  }
-
-  void print() const {
-    std::cout << "-- Memory (" << this->data.size() << ") --" << std::endl;
-
-    for (uint64_t i = 0; i < this->data.size(); i++) {
-      std::cout << (uint16_t)this->data[i] << std::endl;
-    }
-  }
+  void print() const;
 };
